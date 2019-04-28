6f819dd2259f671fae00bf82ec669a9b9e433a03
ANNEMARIE OSULLIVAN <annemarieos.irl@gmail.com>
2019-04-28 12:07:14 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-my_name => "Anne Marie"
+"my_name" => "Anne Marie"
 
 end
 
